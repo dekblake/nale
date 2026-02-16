4fc95353453506d00e6ea05d062a6efcbfc94a45
dekblake <djuanblake@gmail.com>
2026-02-16 14:36:56 +0000

    initial setup

---
+++ .devcontainer/setup_flutter.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# Install dependencies
+sudo apt-get update
+sudo apt-get install -y curl unzip xz-utils git
+
+# Install Flutter
+cd /opt
+sudo git clone https://github.com/flutter/flutter.git -b stable
+sudo chown -R codespace:codespace /opt/flutter
+
+# Add Flutter to PATH permanently
+echo 'export PATH="$PATH:/opt/flutter/bin"' >> ~/.bashrc
+export PATH="$PATH:/opt/flutter/bin"
+
+# Pre-cache web
+flutter precache --web
+flutter config --enable-web
+
+flutter doctor
